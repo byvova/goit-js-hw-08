de013e772f01346a63483c16af0b715d08742fe7
blyavova <grushvo3@gmail.com>
2023-10-08 13:30:22 +0300

    Wow

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,8 @@
+import simplelightbox from 'https://cdn.jsdelivr.net/npm/simplelightbox@2.14.2/+esm'
+
+
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
