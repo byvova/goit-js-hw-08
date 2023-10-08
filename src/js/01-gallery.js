86bc7dce691c03806fe2a96db457f085116b551f
blyavova <grushvo3@gmail.com>
2023-10-08 13:42:27 +0300

    modern normalize

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,31 @@ import simplelightbox from 'https://cdn.jsdelivr.net/npm/simplelightbox@2.14.2/+
 
 
 // Add imports above this line
-// import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
+
+const listGallery = document.querySelector('.gallery');
+
+const galleryElements = [];
+galleryItems.forEach(item => {
+    const element = `<li>
+    <a class="gallery__item"
+        href="${item.original}">
+        <img class="gallery__image"
+        src="${item.preview}" 
+        alt="${item.description}" />
+    </a>
+    </li>`;
+    galleryElements.push(element);
+});
+listGallery.innerHTML = galleryElements.join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
